1134697c81eeb4fde4db74960dee00d40c5f4d93
A. Emre Ünal <aemreunal@gmail.com>
2014-03-04 00:02:04 +0200

    Initial commit.

---
+++ CS320-Project1-TicTacToe/src/Controller/Hello.java
@@ -0,0 +1,14 @@
+package Controller;
+
+/*
+ * This code belongs to:
+ * Ahmet Emre Unal
+ * S001974
+ * emre.unal@ozu.edu.tr
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
