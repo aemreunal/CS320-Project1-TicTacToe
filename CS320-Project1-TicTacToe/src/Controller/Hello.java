ced0d8c0f4986b8793fee7e91f7e2177930a881c
Deniz Sökmen <deniz.sokmen@ozyegin.edu.tr>
2014-03-04 13:58:15 +0200

    Added xxxxx

--- CS320-Project1-TicTacToe/src/Controller/Hello.java
+++ CS320-Project1-TicTacToe/src/Controller/Hello.java
@@ -13,5 +13,6 @@ public class Hello {
 		System.out.println("This is AEU.");
 		System.out.println("This, too, is AEU.");
 		System.out.println("Again, AEU.");
+		System.out.println("XXXXXXXXXXXXXXXX");
 	}
 }
