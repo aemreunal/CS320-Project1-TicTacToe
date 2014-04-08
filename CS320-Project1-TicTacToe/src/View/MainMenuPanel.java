705a57b825f68ac1857f640b42f2c9b1b7b503c0
Eren Sezener <erensezener@gmail.com>
2014-04-08 20:26:03 +0300

    Add MainMenuPanel implementation

--- CS320-Project1-TicTacToe/src/View/MainMenuPanel.java
+++ CS320-Project1-TicTacToe/src/View/MainMenuPanel.java
@@ -1,7 +1,7 @@
 package View;
 
-import javax.swing.JPanel;
-
+import javax.swing.*;
+import Controller.Controller;
 /*
  * This code belongs to:
  * Ahmet Emre Unal
@@ -10,5 +10,19 @@ import javax.swing.JPanel;
  */
 
 public class MainMenuPanel extends JPanel {
-	
+    private JButton localGameButton;
+    private JButton remoteGameButton;
+    private Controller controller;
+
+    public MainMenuPanel(Controller controller){
+        this.controller = controller;
+        this.localGameButton = createButton("Start Local Game");
+        this.remoteGameButton = createButton("Start Remote Game");
+    }
+
+    private JButton createButton(String buttonString){
+        JButton button = new JButton(buttonString);
+        this.add(button);
+        return button;
+    }
 }
