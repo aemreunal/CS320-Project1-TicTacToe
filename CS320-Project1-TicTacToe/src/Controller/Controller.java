3245a357f4d46d80298c26b50629f73da0c620cd
A. Emre Ünal <aemreunal@gmail.com>
2014-04-08 19:47:27 +0300

    Update fields of controller.

--- CS320-Project1-TicTacToe/src/Controller/Controller.java
+++ CS320-Project1-TicTacToe/src/Controller/Controller.java
@@ -1,11 +1,10 @@
 package Controller;
 
-import javax.swing.JPanel;
-
 import Model.GameLogic;
 import Model.GameStatus;
 import Model.NetworkAdapter;
 import Model.Winner;
+import View.GameWindow;
 
 /*
  * This code belongs to:
@@ -15,12 +14,10 @@ import Model.Winner;
  */
 
 public class Controller {
-	private JPanel mainMenuPanel;
-	private JPanel remoteGameChoicePanel;
-	private JPanel gameBoard;
+	private GameWindow gameWindow;
 	private NetworkAdapter netAdapter;
 	private GameLogic gameModel;
-	private GameStatus status;
+	private GameStatus status = GameStatus.NOT_RUNNING;
 	
 	public static void main(String[] args) {
 		new Controller();
